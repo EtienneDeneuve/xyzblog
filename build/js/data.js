b15c8b84a7a893174c2022a94c12fdeb74f2acfb
Etienne Deneuve <etienne@deneuve.xyz>
2020-02-27 12:30:41 +0100

    add build

---
+++ build/js/data.js
@@ -0,0 +1,354 @@
+[{
+  "title": "Seven: An Eleventy Template",
+  "date" : "Feb 27, 2020",
+  "summary" : "A Bootstrap Based template for the awesome Eleventy static site generator.",
+  
+  "url": "/projects/seven/",
+  "tags" : ""
+},{
+  "title": "Building",
+  "date" : "Feb 27, 2020",
+  
+  
+  "url": "/projects/seven/06_Building/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "Configuration",
+  "date" : "Feb 27, 2020",
+  
+  
+  "url": "/projects/seven/05_Configuration/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "Site",
+  "date" : "Feb 27, 2020",
+  "summary" : "A markdown first slide framework based on the awesome reveal.js, with multiple presentations, custom layouts and a whole lot more.&quot;",
+  
+  "url": "/projects/seven/04_Site/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "Scripts",
+  "date" : "Feb 27, 2020",
+  
+  
+  "url": "/projects/seven/03_Scripts/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "Seven: Styles",
+  "date" : "Feb 27, 2020",
+  
+  
+  "url": "/projects/seven/02_Styles/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "RavVeal: Introduction",
+  "date" : "Feb 27, 2020",
+  "summary" : "A markdown first slide framework based on the awesome reveal.js, with multiple presentations, custom layouts and a whole lot more.&quot;",
+  
+  "url": "/projects/rayveal/",
+  "tags" : ""
+},{
+  "title": "Templates",
+  "date" : "Feb 27, 2020",
+  "summary" : "A markdown first slide framework based on the awesome reveal.js, with multiple presentations, custom layouts and a whole lot more.&quot;",
+  
+  "url": "/projects/rayveal/04_Templates/",
+  "tags" : ""
+},{
+  "title": "Features",
+  "date" : "Feb 27, 2020",
+  "summary" : "A markdown first slide framework based on the awesome reveal.js, with multiple presentations, custom layouts and a whole lot more.&quot;",
+  
+  "url": "/projects/rayveal/03_Features/",
+  "tags" : ""
+},{
+  "title": "Setup Instructions",
+  "date" : "Feb 27, 2020",
+  "summary" : "A markdown first slide framework based on the awesome reveal.js, with multiple presentations, custom layouts and a whole lot more.&quot;",
+  
+  "url": "/projects/rayveal/02_Instructions/","icon": "<i class=\"fas fa-project-diagram\"></i>",
+  "tags" : "project,rayveal"
+},{
+  "title": "Terraform AzureRM 2.0 Provider",
+  "date" : "Feb 24, 2020",
+  
+  
+  "url": "/posts/2020-02-25-terraform-azurerm-2/",
+  "tags" : "Azure,PowerShell,Azure Devops,Cloud,Tests"
+},{
+  "title": "Testing your infra my easy way 2",
+  "date" : "Feb 16, 2020",
+  "summary" : "Testing your infra shouldn&#39;t be hard. Try my easy way.",
+  
+  "url": "/posts/2020-02-17-infra-testing-easy-path-2/",
+  "tags" : "Azure,PowerShell,Azure Devops,Cloud,Tests"
+},{
+  "title": "IaC &amp; Tests",
+  "date" : "Feb 06, 2020",
+  "summary" : "I was looking for the easiest way to test Azure infrastructure. I wanted to find something easier (or lazier).",
+  
+  "url": "/posts/2020-02-07-infra-testing-easy-path/",
+  "tags" : "Azure,PowerShell,Azure Devops,Cloud,Tests"
+},{
+  "title": "Lancement de Powershell du Zéro",
+  "date" : "Jan 22, 2019",
+  
+  
+  "url": "/2019/01/22/lancement-de-powershell-du-zero/",
+  "tags" : ""
+},{
+  "title": "How to install Azure Stack Dev kit in a breeze
+",
+  "date" : "Dec 05, 2018",
+  
+  
+  "url": "/2018/12/05/adsk-in-a-breeze/",
+  "tags" : ""
+},{
+  "title": "Some Az Cli 2.0 bulk commands
+",
+  "date" : "Jul 02, 2018",
+  
+  
+  "url": "/2018/07/02/az-cli-20-tricks/",
+  "tags" : ""
+},{
+  "title": "Dynamic update of Bind9 server in Azure with Terraform !
+",
+  "date" : "Jul 02, 2018",
+  
+  
+  "url": "/2018/07/02/how-to-manage-dns-terraform/",
+  "tags" : ""
+},{
+  "title": "Git pour les Ops, par un ops le retour",
+  "date" : "Jun 28, 2018",
+  
+  
+  "url": "/2018/06/28/git-pour-les-ops-par-un-ops-le-retour/",
+  "tags" : ""
+},{
+  "title": "Documentation as Code",
+  "date" : "Jun 26, 2018",
+  
+  
+  "url": "/2018/06/26/documentation-as-code/",
+  "tags" : ""
+},{
+  "title": "Open Source + Windows = Vs Code + Bash + Git
+",
+  "date" : "Jun 26, 2018",
+  
+  
+  "url": "/2018/06/26/setup-vs-code-bash-git/",
+  "tags" : ""
+},{
+  "title": "Git pour Ops, par un Ops",
+  "date" : "Jun 23, 2018",
+  
+  
+  "url": "/2018/06/23/git-pour-ops-par-un-ops/",
+  "tags" : ""
+},{
+  "title": "Terraform: Providers à connaître, DataSource à redécouvrir !",
+  "date" : "Apr 06, 2018",
+  
+  
+  "url": "/2018/04/06/terraform-providers-datasource/",
+  "tags" : ""
+},{
+  "title": "Os Factory Azure et Aws en Open Source avec Microsoft et Société Générale !
+",
+  "date" : "Mar 28, 2018",
+  
+  
+  "url": "/2018/03/28/os-factory-azure-et-aws/",
+  "tags" : ""
+},{
+  "title": "Microsoft Expérience 17 – VSTS For OPS part 3 !
+",
+  "date" : "Oct 25, 2017",
+  
+  
+  "url": "/2017/10/25/vsts-for-ops-part-3/",
+  "tags" : ""
+},{
+  "title": "Microsoft Expérience 17 – VSTS For OPS part 2 !
+",
+  "date" : "Oct 13, 2017",
+  
+  
+  "url": "/2017/10/13/vsts-for-ops-2/",
+  "tags" : ""
+},{
+  "title": "Microsoft Expérience 17 &amp;#8211; VSTS For OPS part 1 !",
+  "date" : "Oct 09, 2017",
+  
+  
+  "url": "/2017/10/09/vsts-for-ops-1/",
+  "tags" : ""
+},{
+  "title": "Microsoft Experience 17 : Infrastructure as Code : modélisez et provisionnez vos services Azure avec Terraform et Packer",
+  "date" : "Oct 01, 2017",
+  
+  
+  "url": "/2017/10/01/microsoft-experience-17-infrastructure-code-modelisez-et-provisionnez-vos-services-azure-avec-terraform-et-packer/",
+  "tags" : ""
+},{
+  "title": "MVP !",
+  "date" : "Oct 01, 2017",
+  
+  
+  "url": "/2017/10/01/mvp/",
+  "tags" : ""
+},{
+  "title": "Mon premier it.cast avec Maxime et Pascal (et les autres) !
+",
+  "date" : "Sep 29, 2017",
+  
+  
+  "url": "/2017/09/29/mon-premier-itcast/",
+  "tags" : ""
+},{
+  "title": "Cell&amp;#8217;insights &amp;#8211; &amp;#8220;ISV, Journey to the Cloud&amp;#8221; dans les bacs!",
+  "date" : "Sep 27, 2017",
+  
+  
+  "url": "/2017/09/27/cellinsights-isv-journey-cloud-dans-les-bacs/",
+  "tags" : ""
+},{
+  "title": "Azure Powershell &amp;#8211; Nettoyer les Tags",
+  "date" : "Sep 27, 2017",
+  
+  
+  "url": "/2017/09/27/azure-powershell-nettoyer-les-tags/",
+  "tags" : ""
+},{
+  "title": "FRPSUG &amp;#8211; Powershell Saturday et contenu de la session (de l&amp;#8217;utile, ou pas)",
+  "date" : "Sep 26, 2017",
+  
+  
+  "url": "/2017/09/27/frpsug-powershell-saturday-et-contenu-de-la-session-de-lutile-ou-pas/",
+  "tags" : ""
+},{
+  "title": "Azure Powershell &amp;#8211; Comment on fait quand ca n&amp;#8217;existe pas ?",
+  "date" : "Sep 26, 2017",
+  
+  
+  "url": "/2017/09/26/azurepscmdnotfound/",
+  "tags" : ""
+},{
+  "title": "[Update] Azure Cloud Shell &amp;#8211; Pleins de nouveautés ! (Et du gros) &amp;#8211; Powershell is Coming !",
+  "date" : "Sep 25, 2017",
+  
+  
+  "url": "/2017/09/25/azure-cloud-shell-pleins-de-nouveautes-et-du-gros/",
+  "tags" : ""
+},{
+  "title": "[update] Project Honolulu Public Preview &amp;#8211; Mon retour",
+  "date" : "Sep 24, 2017",
+  
+  
+  "url": "/2017/09/24/project-honolulu-public-preview-mon-retour/",
+  "tags" : ""
+},{
+  "title": "Azure Networking – TP – Part 2 &amp;#8211; vNet Peering",
+  "date" : "Apr 27, 2017",
+  
+  
+  "url": "/2017/04/27/azure-networking-tp-part-2/",
+  "tags" : ""
+},{
+  "title": "Azure Networking &amp;#8211; TP &amp;#8211; Part 1 &amp;#8211; vNets &amp;#038; Subnets",
+  "date" : "Apr 25, 2017",
+  
+  
+  "url": "/2017/04/25/azure-networking-tp-part-1/",
+  "tags" : ""
+},{
+  "title": "Azure et le backup ? (d&amp;#8217;un Mac)",
+  "date" : "Mar 14, 2017",
+  
+  
+  "url": "/2017/03/14/azure-et-le-backup-dun-mac/",
+  "tags" : ""
+},{
+  "title": "Vorlon.js : Un petit billet pour un gentil copain dev !",
+  "date" : "Feb 02, 2017",
+  
+  
+  "url": "/2017/02/03/vorlon-js-un-petit-billet-pour-un-gentil-copain-dev/",
+  "tags" : ""
+},{
+  "title": "Installation de Nginx en reverse proxy avec réecriture du HTML
+",
+  "date" : "Feb 02, 2017",
+  
+  
+  "url": "/2017/02/02/installation-de-nginx-en-reverse-proxy-avec-reecriture-du-html/",
+  "tags" : ""
+},{
+  "title": "Agent Oms sur Linux dans Azure",
+  "date" : "Feb 02, 2017",
+  
+  
+  "url": "/2017/02/02/agent-oms-sur-linux-dans-azure/",
+  "tags" : ""
+},{
+  "title": "[Updated 20/03/2017] Visual Studio Code pour Ansible, Terraform",
+  "date" : "Jan 26, 2017",
+  
+  
+  "url": "/2017/01/26/visual-studio-code-pour-ansible-terraform/",
+  "tags" : ""
+},{
+  "title": "Utiliser Ansible et Azure, oui c&amp;#8217;est possible !",
+  "date" : "Jan 26, 2017",
+  
+  
+  "url": "/2017/01/26/utiliser-ansible-et-azure-oui-cest-possible/",
+  "tags" : ""
+},{
+  "title": "Get ink level from HP printers in Powershell
+",
+  "date" : "Jan 15, 2016",
+  
+  
+  "url": "/2016/01/15/get-ink-level-from-hp-printers-in-powershell/",
+  "tags" : ""
+},{
+  "title": "Side loading application without SCCM &amp;#8211; Part 3",
+  "date" : "Jan 11, 2016",
+  
+  
+  "url": "/2016/01/11/side-loading-application-without-sccm-part-3/",
+  "tags" : ""
+},{
+  "title": "Side loading application without SCCM &amp;#8211; Part 2",
+  "date" : "Jan 11, 2016",
+  
+  
+  "url": "/2016/01/11/side-loading-application-without-sccm-part-2/",
+  "tags" : ""
+},{
+  "title": "Side loading application without SCCM &amp;#8211; Part 1",
+  "date" : "Jan 11, 2016",
+  
+  
+  "url": "/2016/01/11/side-loading-application-without-sccm-part-1/",
+  "tags" : ""
+},{
+  "title": "Get-NextTrain !",
+  "date" : "Dec 16, 2015",
+  
+  
+  "url": "/2015/12/16/get-nexttrain/",
+  "tags" : ""
+},{
+  "title": "Creating an Adaptive Web Site for Multiple Screens",
+  "date" : "Nov 18, 2011",
+  "summary" : "How to build a web site that automatically adapts its layout to various screen sizes, orientations, and resolutions on desktop browsers and mobile devices.",
+  "thumbnail": "/images/courses/adaptive_tn.png",
+  "url": "https://linkedin-learning.pxf.io/c/1252977/449670/8005?u=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Fcreating-an-adaptive-web-site-for-multiple-screens",
+  "tags" : "css,javascript,html,web designer,web developer"
+}]
